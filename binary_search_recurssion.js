f44c2cd1ac5bd31b3f9fa0d1d1bcdc76148e1099
Vishnu P S <vishnu13three@gamil.com>
2024-10-01 19:19:23 +0530

    binary search using recurssion

---
+++ binary_search_recurssion.js
@@ -0,0 +1,27 @@
+function binarySearch(arr,target,left,right) {
+    if(left > right){
+        return "not found";
+    }
+
+    let mid = Math.floor((left+right)/2);
+
+
+    if(arr[mid] === target) {
+        return mid;
+    }
+    if( target > arr[mid] ){
+        return binarySearch(arr,target,mid+1, right);
+    }else{
+        return binarySearch(arr,target,left,mid - 1)
+    }
+}
+
+let arr = [1,2,3,4,5,6,7,8,9];
+
+let target = 12;
+
+console.log(arr.length);
+
+let result = binarySearch(arr, target, 0, arr.length - 1);
+
+console.log(result);
