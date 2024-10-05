3e2bd952a83905b0c91d556a266a2bb2578ace7f
Vishnu P S <vishnu13three@gamil.com>
2024-10-05 11:37:32 +0530

    two sums

---
+++ two_sum_problem.js
@@ -0,0 +1,27 @@
+
+// 1. Two Sum Problem:
+// Problem Statement:
+// Given an array of integers, return the indices of the two numbers that add up to a specific target.
+
+// Input: nums = [2, 7, 11, 15], target = 9
+// Output: [0, 1] 
+// Explanation: Because nums[0] + nums[1] = 2 + 7 = 9
+
+function twoSum ( nums, target) {
+    let map =  new Map();
+    for(let i = 0 ; i< nums.length; i++){
+        let compliment = target - nums[i];
+
+        if(map.has(compliment)){
+            return [map.get(compliment),i]
+        }
+        map.set(nums[i],i);
+    }
+    return [];
+}
+
+
+const nums = [ 2, 7 , 11, 15];
+const target = 9;
+
+console.log(twoSum(nums,target));
