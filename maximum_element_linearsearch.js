41083835e4394b6b12a16837850bb5028b1a7a2d
Vishnu P S <vishnu13three@gamil.com>
2024-10-06 20:56:01 +0530

    find maximum element linear search

--- maximum_element_linearsearch.js
+++ maximum_element_linearsearch.js
@@ -2,10 +2,19 @@
 // Problem Statement:
 // Given an array of integers arr, write a function to find the maximum element in the array using linear search.
 
-function maximumElement (arr){
+function findMaximum (arr){
     if(arr.length === 0){
         return null;
     }
 
     let max = arr [0];
+    for (let i = 1; i< arr.length; i++){
+        if(arr[i] > max){
+            max = arr[i];
+        }
+    }
+    return max;
 }
+
+const arr = [10, 25, 6, 9, 15, 34, 7];
+console.log(findMaximum(arr)); 
